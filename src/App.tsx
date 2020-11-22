0e40728a91bc8784993fb18374c2517fa585adc1
Velenir <velenir@gmail.com>
2020-11-22 17:01:45 +0300

    setup paraswap-widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { PSWidget } from 'paraswap-widget';
+import 'paraswap-widget/dist/PSWidget.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <PSWidget
+          referrer="my_company"
+          providerUrl={'https://mainnet.infura.io/v3/3c9b697bcf414df8b2e59f7f5523a93a' + process.env.INFURA_ID}
+          unlimitedAllowance={false}
+          bgColor={'#DDD'}
+          defaultPair={{ from: 'ETH', to: 'DAI', amount: '1' }}
+        />
       </header>
     </div>
   );
